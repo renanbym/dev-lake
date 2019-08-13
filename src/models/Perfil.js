16450db6c38fb536aecb5be50525abb547f5cc7f
Renan Mariano <renanbym@gmail.com>
2019-08-13 16:07:53 +0100

    update profile

--- src/models/Perfil.js
+++ src/models/Perfil.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
 
 const modelSchema = new mongoose.Schema({
-    url: { type: String, require: true },
+    url: { type: String, require: true, index: { unique: true } },
     status: { type: String, enum: ['created', 'updated', 'modernize'], require: true },
     name: String,
     headline: String,
     location: String,
     summary: String,
     positions: [{
+        _id: false,
         title: { type: String },
         company: { type: String },
         description: { type: String },
         date1: { type: String },
         date2: { type: String },
         roles: [{
+            _id: false,
             title: { type: String },
             company: { type: String },
             description: { type: String },
@@ -22,12 +24,14 @@ const modelSchema = new mongoose.Schema({
         }]
     }],
     educations: [{
+        _id: false,
         title: String,
         degree: String,
         date1: Date,
         date2: Date
     }],
     skills: [{
+        _id: false,
         title: String,
         count: Number
     }],
--- src/services/update-profile.js
+++ src/services/update-profile.js
@@ -1,27 +1,40 @@
-'use strict';
-
 const scrapedin = require('scrapedin')
-const config = require('./config')
+const config = require('../../config')
+const { connect } = require('../_core/connection')
+const Perfil = require('../models/Perfil')
+const { ObjectId } = require('mongodb')
 
-const handler = (event, context, callback) => {
+const parseBody = (event) => {
 
-  try {
-    const data = JSON.parse(event.body);
-
-    console.log('begin')
-    const profileScraper = await scrapedin(config.linkedin)
-    const profile = await profileScraper(data.url);
-
-    console.log(profileScraper);
-    console.log(profile);
-
-    return callback(null, { statusCode: 200, body: JSON.stringify({ pong: 'node' }) });
-  } catch (e) {
-    
-    return callback(null, { statusCode: 400, body: JSON.stringify({ error: error.message }) });
+  if (event.isOffline) {
+    return event
+  } else {
+    return JSON.parse(event.body)
   }
 
-};
+}
+
+const handler = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false
+
+  try {
+    const data = parseBody(event)
+
+    await connect(true)
+
+    let perfilData = await Perfil.findById(new ObjectId(data.id))
+
+    const profileScraper = await scrapedin(config.linkedin)
+    const profile = await profileScraper(perfilData.url)
+
+    perfilData = await Perfil.findOneAndUpdate({ _id: new ObjectId(data.id) }, { $set: Object.assign({}, profile, profile.profileAlternative) }, { new: true });
+
+    return callback(null, { statusCode: 200, body: JSON.stringify(perfilData) })
+  } catch (error) {
+    return callback(null, { statusCode: 400, body: JSON.stringify({ error: error.message }) })
+  }
+
+}
 
 
 module.exports.handler = handler
